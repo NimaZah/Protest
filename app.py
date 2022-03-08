49c5fefd7202beff502867c871f9ec3a9655c8d4
Nima Z. Zahedi <43790712+NimaZah@users.noreply.github.com>
2022-03-08 11:43:55 -0500

    experimenting with app

--- app.py
+++ app.py
@@ -11,33 +11,54 @@ temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
 
+# build the path for the trained model
+scoring_columns = ['year','participants','country','region']
+
 path = Path(os.getcwd())
 full_path = os.path.join(path, 'protest.pkl')
 print("path is:", path)
 print("full_path is: ", full_path)
 learner = load_learner(full_path)
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
-@app.route('/predict', methods=['POST'])
-def prediction():
+@app.route('/show-prediction/')
+def show_prediction():
+    ''' 
+    get the scoring parameters entered in home.html and render show-prediction.html
     '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [x for x in request.form.values()]
-    final_features = np.array(int_features)
-    final_features = final_features.reshape(1, -1)
-    prediction = learner.predict(final_features)
-    return render_template('prediction.html', prediction=prediction)
+    # the scoring parameters are sent to this page as parameters on the URL link from home.html
+    # load the scoring parameter values into a dictionary indexed by the column names expected by the model
+    score_values_dict = {}
+    # bring the URL argument values into a Python dictionary
+	# use the scoring parameter values to get a prediction from the model
+    for column in scoring_columns:
+        # use input from home.html for scoring
+        score_values_dict[column] = request.args.get(column)
+    for value in score_values_dict:
+        print("value for "+value+" is: "+str(score_values_dict[value]))
+    # create and load scoring parameters dataframe (containing the scoring parameters)that will be fed into the pipelines
+    score_df = pd.DataFrame(columns=scoring_columns)
+    print("columns are: ")
+    print(scoring_columns)
+    for col in scoring_columns:
+        score_df.at[0,col] = score_values_dict[col]
+    print("score_df is: ")
+    print(score_df)
+    # ensure columns have the correc types    
+    score_df = score_df.astype({"year": int, "participants": int, "country": str, "region": str})
+    print("score_df is: ")
+    print(score_df)
+    # get the model prediction
+    prediction = learner.predict(score_df)
+    print("prediction is: ", prediction[0])
+    # create the return html page
+    return render_template('show-prediction.html', prediction_key=str(prediction[0]))
 
 
 if __name__ == '__main__':
-
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)
